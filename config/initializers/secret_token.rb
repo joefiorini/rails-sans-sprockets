acb0ff9cebffab020fc92d297457630b4ad28eb3
Joe Fiorini <joe@joefiorini.com>
2013-05-07 15:43:36 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NoSprockets::Application.config.secret_token = '64ac3ff693cbf94598dde6aa31afc2e647c900eca5ea42ab3d6a27f6cc70a632439f2f1ae05b7515e5cc01e058b89c404084961f3ea280cda8e89e6a03b45e70'
